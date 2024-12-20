3a08e16b55d9f9b7194a5f1ac63812d486236263
Luiz <lgds.demtrio@gmail.com>
2024-12-20 09:48:00 -0300

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+"""
+Situação:
+
+    Branch main: Contém o código de produção, sem as novas funcionalidades que estamos desenvolvendo.
+    Branch feature-1: Modifica o file1.py para adicionar uma nova funcionalidade.
+    Branch feature-2: Modifica o file2.py para adicionar outra funcionalidade.
+    Branch feature-3: Modifica o file3.py para adicionar uma terceira funcionalidade.
+
+"""
 print("Hello, World")
+print("Adicionando linhas no print")
