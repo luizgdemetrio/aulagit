11677aaab731db21b500b88e6fda4198fd290781
Luiz <lgds.demtrio@gmail.com>
2024-12-22 23:08:01 -0300

    Adicionando file3.py

---
+++ file3.py
@@ -0,0 +1,8 @@
+"""
+Nova feature calculando dois número 
+"""
+
+num1 = int(input("Inserir primeiro número inteiro: "))
+num2 = int(input("Inserir segundo número inteiro: "))
+total = num1 + num2
+print(f"O valor da soma é: {total}")
